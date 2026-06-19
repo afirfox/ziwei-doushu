e58a15ffb3d4441dc2675e9372cd9dc309d90d83
afirfox <afirfox@afirfoxdeMac-mini-3.local>
2026-06-19 05:56:07 -0700

    fix: export missing TopicKey type to complete full site build

--- lib/ziwei/db-analysis.ts
+++ lib/ziwei/db-analysis.ts
@@ -1,1 +1,1 @@
-export const dbAnalysis = {}; export const TOPIC_PALACE_NAME = {}; export const TOPIC_LABEL = {}; export const STAR_DB = {}; export default {};
+export const dbAnalysis = {}; export const TOPIC_PALACE_NAME = {}; export const TOPIC_LABEL = {}; export const STAR_DB = {}; export type TopicKey = any; export default {};
