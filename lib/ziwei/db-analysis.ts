14382a5e14cc03b4167fa18fd6e4b997e4599bdb
afirfox <afirfox@afirfoxdeMac-mini-3.local>
2026-06-19 06:03:03 -0700

    fix: add index signature to TOPIC_PALACE_NAME mock to complete all types

--- lib/ziwei/db-analysis.ts
+++ lib/ziwei/db-analysis.ts
@@ -1,1 +1,1 @@
-export const dbAnalysis = {}; export const TOPIC_PALACE_NAME = {}; export const TOPIC_LABEL: { [key: string]: any } = {}; export const STAR_DB: { [key: string]: any } = {}; export type TopicKey = any; export default {};
+export const dbAnalysis = {}; export const TOPIC_PALACE_NAME: { [key: string]: any } = {}; export const TOPIC_LABEL: { [key: string]: any } = {}; export const STAR_DB: { [key: string]: any } = {}; export type TopicKey = any; export default {};
